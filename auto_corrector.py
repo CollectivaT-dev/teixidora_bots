fd28522d0e507972f52c696621eec3ccb479c210
gullabi <bkulebi@gmail.com>
2020-04-16 17:28:57 +0200

    placeholder auto corrector logic added

---
+++ auto_corrector.py
@@ -0,0 +1,9 @@
+import logging
+
+class AutoCorrector(object):
+    def __init__(self):
+        pass
+
+    def auto_correct(self, responses):
+        corrected_content = 'placeholder'
+        return corrected_content
--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import re
 import json
 import time
@@ -7,6 +8,7 @@ import logging
 import pywikibot
 
 from pylanguagetool import api
+from auto_corrector import AutoCorrector
 
 LT_URL = 'https://languagetool.org/api/v2/'
 
@@ -15,6 +17,7 @@ def main(title):
 
     c_bot.get_page(title)
     c_bot.correct_notes()
+    c_bot.implement_corrections()
 
 class Bot(object):
     def __init__(self, botname, languagetool = LT_URL):
@@ -22,12 +25,22 @@ class Bot(object):
         self.site = pywikibot.Site('ca', 'teixidora')
         self.botname = botname
         self.languagetool = LT_URL
+        self.outname = None
+
+        self.auto_corrector = AutoCorrector()
 
     def get_page(self, title):
         # get a new teixidora page initializing the rest of the variables
         self.title = title
         self.page = pywikibot.Page(self.site, title)
 
+        # get cache out file hash
+        # TODO push to a db and use hash as the key
+        h = hashlib.md5(self.title.encode('utf8'))
+        self.outname = h.hexdigest()+'.json'
+        self.outpath = 'cache/'+self.outname # TODO better path management
+
+        # clean the notes and corrected notes objects if they were full
         self.notes = []
         self.corrected_notes = {}
 
@@ -46,7 +59,7 @@ class Bot(object):
         content = note_page.text
         language = self.get_language(content)
         # TODO send the content to be corrected according to the LT rules
-        corrected_content = self.correct_content(content, language)
+        return self.correct_content(content, language)
 
     def get_language(self, content):
         # placeholder
@@ -57,45 +70,67 @@ class Bot(object):
         # Segments and sends the content to LT according to the
         # public api rate limits
         # http://wiki.languagetool.org/public-http-api
-        per_minute_size_limit = 60e3 #KB
-        per_req_size_limit = 10e3 # KB
-        per_minute_req_limit = 12 # per minute
-        sentences = content.split('. ')
-        requests = []
-        chunk = []
-        for sentence in sentences:
-            chunk.append(sentence)
-            total_chunk = '. '.join(chunk)
-            if sys.getsizeof(total_chunk) > per_req_size_limit:
-                requests.append(total_chunk)
-                chunk = []
-        if chunk:
-            # add last chunk
-            requests.append('. '.join(chunk))
 
-        # send requests to api
-        # TODO smarted rate limit control needed
-        responses = {'title': self.title, 'results': []}
-        total_requests = len(requests)
-        for i, request in enumerate(requests):
-            response = api.check(request,
-                                 api_url=self.languagetool,
-                                 lang=language)
-            # TODO check language, if confidence lower than 0.90 resend
-           
-            message = '%i/%i response sent'%(i+1, total_requests)
-            print(message)
-            logging.info(message)
-            if i+1 != total_requests:
-                # wait at all except the last LT api call
-                time.sleep(4)
-            responses['results'].append({'content': request,
-                                           'response': response})
+        if os.path.isfile(self.outpath):
+            msg = 'title exists in cache: %s'%self.title
+            print(msg)
+            logging.info(msg)
+            with open(self.outpath) as f:
+                responses = json.load(f)
+            return responses
+        else:
+            per_minute_size_limit = 60e3 #KB
+            per_req_size_limit = 10e3 # KB
+            per_minute_req_limit = 12 # per minute
+            sentences = content.split('. ')
+            requests = []
+            chunk = []
+            for sentence in sentences:
+                chunk.append(sentence)
+                total_chunk = '. '.join(chunk)
+                if sys.getsizeof(total_chunk) > per_req_size_limit:
+                    requests.append(total_chunk)
+                    chunk = []
+            if chunk:
+                # add last chunk
+                requests.append('. '.join(chunk))
 
-        h = hashlib.md5(self.title.encode('utf8'))
-        outname = h.hexdigest()+'.json'
-        with open('cache/'+outname, 'w') as out:
-            json.dump(responses, out, indent = 2)
+            # send requests to api
+            # TODO smarter rate limit control needed
+            responses = {'title': self.title, 'results': []}
+            total_requests = len(requests)
+            for i, request in enumerate(requests):
+                response = api.check(request,
+                                     api_url=self.languagetool,
+                                     lang=language)
+                # TODO check language, if confidence lower than 0.90 resend
+               
+                message = '%i/%i response sent'%(i+1, total_requests)
+                print(message)
+                logging.info(message)
+                if i+1 != total_requests:
+                    # wait at all except the last LT api call
+                    time.sleep(4)
+                responses['results'].append({'content': request,
+                                               'response': response})
+            with open(self.outpath, 'w') as out:
+                json.dump(responses, out, indent = 2)
+            return responses
+
+    def implement_corrections(self):
+        if self.corrected_notes:
+            # implements the corrections and pushes the results in
+            # self.corrected_notes[note]['results'][i]['corrected_content']
+            for url, responses in self.corrected_notes.items():
+                for result in responses['results']:
+                    final_corrected_content =\
+                                   self.auto_corrector.auto_correct(result)
+                    result['corrected_content'] = final_corrected_content
+                with open(self.outpath.replace('.json', '_c.json'), 'w') as out:
+                    json.dump(responses, out, indent = 2)
+        else:
+            msg = 'no corrections found for %s'%self.title
+            logging.warning(msg)
 
 if __name__ == "__main__":
     title = sys.argv[1] 

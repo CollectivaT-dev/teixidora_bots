2d93e4c24f96d5204d7b0bd993d36dd40c0df0ae
gullabi <bkulebi@gmail.com>
2020-05-25 17:39:25 +0200

    new auto cluster steps added

--- scripts/dictionary_cluster.py
+++ scripts/dictionary_cluster.py
@@ -10,29 +10,53 @@ def main():
     # TODO get info directly from teixidora
     data = json.load(open('cache/global_corpora.json'))
     print('calculating organizations')
-    org_groups = cluster(data['organizations'])
+    org_groups, org_keys = cluster(data['organizations'])
     print('calculating projects')
-    pro_groups = cluster(data['projects'])
+    pro_groups, pro_keys = cluster(data['projects'])
     print('calculating people')
-    ppl_groups = cluster(data['people'])
+    ppl_groups, ppl_keys = cluster(data['people'])
 
     results = {'organizations': org_groups,
                'projects': pro_groups,
                'people': ppl_groups}
+    key_results = {'organizations': org_keys,
+                   'projects': pro_keys,
+                   'people': ppl_keys}
 
-    with open('cache/cluster.json', 'w') as out:
+    with open('cache/cluster_lists.json', 'w') as out:
         json.dump(results, out, indent=2)
+    with open('cache/cluster_keys.json', 'w') as out:
+        json.dump(key_results, out, indent=2)
+
+    new_org_groups, new_org_keys = auto_assign(org_groups)
+    new_pro_groups, new_pro_keys = auto_assign(pro_groups)
+
+    results = {'organizations': new_org_groups,
+               'projects': new_pro_groups,
+               'people': ppl_groups}
+    key_results = {'organizations': new_org_keys,
+                   'projects': new_pro_keys,
+                   'people': ppl_keys}
+
+    with open('cache/auto_cluster_lists.json', 'w') as out:
+        json.dump(results, out, indent=2)
+    with open('cache/auto_cluster_keys.json', 'w') as out:
+        json.dump(key_results, out, indent=2)
+
 
 def cluster(data):
     similarity_m = calculate_similarity(data)
     db = DBSCAN(eps=0.1, min_samples=1, metric='precomputed').fit(similarity_m)
     groups = {}
+    keys = {}
     for group in list(set(db.labels_)):
         groups[group] = []
 
     for group, name in zip(db.labels_, data):
         groups[group].append(name)
-    return list(groups.values())
+        keys[name] = int(group)
+    # TODO group id -1 (noise) treatment
+    return list(groups.values()), keys
 
 def calculate_similarity(data):
     mm = np.zeros(shape=(len(data),len(data)))
@@ -50,6 +74,42 @@ def pre_filter(string):
     new = re.sub('·|\.$','',string)
     new = re.sub('l\.l', 'll', new)
     return unidecode.unidecode(new).lower()
+
+def auto_assign(groups):
+    ref = 0
+    count = 0
+    new_groups = []
+    new_keys = {}
+    for group in groups:
+        if len(group) > 1:
+            chars = [len(re.sub('\(.+\)','',g).strip())\
+                     for g in list(group)]
+            ref += 1
+            if max(chars) - min(chars) < 6:
+                count += 1
+                new_groups.append(group)
+            else:
+                # append each element separately
+                # or group into subgroups of equal lengths
+                subgroups = {}
+                for element in group:
+                    length = len(re.sub('\(.+\)','',element).strip())
+                    if not subgroups.get(length):
+                        subgroups[length] = []
+                    subgroups[length].append(element)
+                for elements in subgroups.values():
+                    new_groups.append(elements)
+                    if len(elements) > 1:
+                        count += 1
+        else:
+            new_groups.append(group)
+
+    for i, group in enumerate(new_groups):
+        for name in group:
+            new_keys[name] = i
+
+    print('%i out of %i automatically assigned'%(count, ref))
+    return new_groups, new_keys
 
 if __name__ == "__main__":
     main()
